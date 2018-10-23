6a467e78cad761a43bd7cd13afb3879cbd15440b
startrekdiscord <43800997+startrekdiscord@users.noreply.github.com>
2018-10-23 14:53:45 -0400

    Adding invite ban

---
+++ invite_ban/invite_ban.py
@@ -0,0 +1,34 @@
+import discord
+
+
+class InviteBan:
+    def __init__(self, bot):
+        self.bot = bot
+        self.badlist = ["discord.gg", "twitch.tv", "twitter.com"]
+
+    async def _check_name_for_invite(self, member):
+        """
+            Looks for discord.gg etc anywhere in a user's name or in their
+            server-specific name, and immediately ban them if it does
+        """
+        if any(n in member.display_name.lower() for n in self.badlist):
+            await self.bot.ban(member)
+
+    async def on_member_join(self, member):
+        """ check every user when they join """
+        await self._check_name_for_invite(member)
+
+    async def on_member_update(self, before, after):
+        """ check every user when they change their name """
+        await self._check_name_for_invite(after)
+
+    async def on_message(self, message):
+        """find welcome messages (or mentions) and delete them too"""
+        if message.mentions:
+            user = message.mentions[0]
+            if any(n in user.display_name.lower() for n in self.badlist):
+                await self.bot.delete_message(message)
+
+
+def setup(bot):
+    bot.add_cog(InviteBan(bot))
