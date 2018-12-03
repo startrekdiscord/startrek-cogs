14c5f6bb9ad071a042062ff39debb44e4ffe38f2
Tim Huff <timhuff@gmail.com>
2018-12-02 18:11:31 -0600

    migrated to version 3

---
+++ time/__init__.py
@@ -0,0 +1,4 @@
+from .index import Timecog
+
+def setup(bot):
+    bot.add_cog(Timecog(bot))
--- time/index.py
+++ time/index.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from redbot.core import commands
 import dateparser
 from dateutil.tz import tzlocal
 import sqlite3
@@ -9,7 +9,7 @@ import time
 GOOGLE_MAPS_API_KEY = 'AIzaSyDnuQ_OgMeVMIZdUT252SAvjgC__Hsv-N8'
 
 
-class Timecog:
+class Timecog(commands.Cog):
     """Cog for handling time communication"""
 
     def __init__(self, bot):
@@ -23,7 +23,7 @@ class Timecog:
     def __del__(self):
         self.db_connection.close()
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ilivein(self, ctx, *, location):
         """Tells the system where you live so it can know what your clock reads"""
 
@@ -33,7 +33,7 @@ class Timecog:
         response = requests.get(api_url).json()
 
         if response['status'] != "OK":
-            await self.bot.say("Unable to parse location: " + location+". Either this isn't a location or we've run out of quota with Google's API")
+            await ctx.send("Unable to parse location: " + location+". Either this isn't a location or we've run out of quota with Google's API")
         else:
             latlng = response['results'][0]['geometry']['location']
             latitude = latlng['lat']
@@ -43,30 +43,30 @@ class Timecog:
             self.db.execute(
                 '''INSERT INTO user_timezones VALUES(?, ?, ?, ?)''', [userId, location, latitude, longitude])
             self.db_connection.commit()
-            await self.bot.say("Location for " +
+            await ctx.send("Location for " +
                                ctx.message.author.display_name+" set to: "+location)
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def timefor(self, ctx, atless_mention):
         """Outputs what a given user's clock reads"""
 
         targetUser = None
-        for member in ctx.message.server.members:
+        for member in ctx.message.guild.members:
             if atless_mention == member.name + '#' + member.discriminator:
                 targetUser = member
         if targetUser is None:
-            await self.bot.say("Unable to locate the specified user: " + atless_mention)
+            await ctx.send("Unable to locate the specified user: " + atless_mention)
             return
         targetUserId = ctx.message.author.id
         self.db.execute(
             '''SELECT * FROM user_timezones WHERE user_id=?''', [targetUserId])
         result = self.db.fetchone()
         if result is None:
-            await self.bot.say("I do not have timezone information for that user.")
+            await ctx.send("I do not have timezone information for that user.")
         else:
             latitude = result[2]
             longitude = result[3]
-            currentEpochTimestamp = int(time.time())
+            currentEpochTimestamp = time.time()
             api_url = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + str(latitude) + \
                 ',' + str(longitude) + '&timestamp=' + \
                 str(currentEpochTimestamp) + '&key=' + GOOGLE_MAPS_API_KEY
@@ -74,24 +74,21 @@ class Timecog:
             dstOffset = response['dstOffset']
             rawOffset = response['rawOffset']
             totalOffset = dstOffset + rawOffset
-            theirTime = dateparser.parse('in '+str(totalOffset)+' minutes')
-            await self.bot.say("The current time for "+targetUser.display_name+" is "+theirTime.strftime("%A @ %I:%M%p (%H:%M)"))
+            theirTime = dateparser.parse(' ' + str(totalOffset) + ' seconds ago UTC')
+            await ctx.send("The current time for "+targetUser.display_name+" is "+theirTime.strftime("%c"))#.strftime("%A @ %I:%M%p (%H:%M)"))
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def time(self, ctx, *, time):
         """Display time in reader's timezone"""
 
         timestamp = dateparser.parse(time)
         if timestamp is None:
-            await self.bot.say("Unable to convert to date/time: " + time)
+            await ctx.send("Unable to convert to date/time: " + time)
         else:
             if timestamp.tzinfo is None:
                 timestamp = timestamp.replace(tzinfo=tzlocal())
             title = "\""+time+"\" in your local timezone:"
             em = discord.Embed(
                 title=title, timestamp=timestamp)
-            await self.bot.send_message(ctx.message.channel, embed=em)
+            await ctx.send(embed=em)
 
-
-def setup(bot):
-    bot.add_cog(Timecog(bot))
