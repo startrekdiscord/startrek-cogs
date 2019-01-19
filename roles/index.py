fb098e51874fe53605a0ed07ff780dbef37472a0
Tim Huff <timhuff@gmail.com>
2019-01-19 09:32:53 -0600

    further protections for manage_guild permissions

--- roles/index.py
+++ roles/index.py
@@ -47,7 +47,7 @@ class Roles(commands.Cog):
 
         targetRole = None
         for role in ctx.guild.roles:
-            if role.name == role_name:
+            if not role.permissions.manage_guild and role.name == role_name:
                 targetRole = role
 
         if targetRole is None:
